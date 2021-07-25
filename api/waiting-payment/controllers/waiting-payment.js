b07b4beb5120b2e9b49546777e75886afe2e4151
shonsirsha <seangeekpro@gmail.com>
2021-07-25 14:53:45 +0700

    new endpoint to check if await payment exists  (and still is) valid

--- api/waiting-payment/controllers/waiting-payment.js
+++ api/waiting-payment/controllers/waiting-payment.js
@@ -4,5 +4,34 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
-
-module.exports = {};
+const calculateHourDifference = (createdAt, today) => {
+  const milliseconds = Math.abs(createdAt - today);
+  const hours = milliseconds / 36e5;
+  return hours;
+}; // returns hour (float)
+module.exports = {
+  async findOneByMeByCourse(ctx) {
+    const { uuid } = ctx.state.user;
+    const { courseId } = ctx.request.body;
+    const pendingPayment = await strapi
+      .query("waiting-payment")
+      .find({ "user.uuid": uuid, "course.id": courseId });
+    if (pendingPayment.length === 1) {
+      const { created_at } = pendingPayment[0];
+      const difference = calculateHourDifference(created_at, new Date());
+      if (difference <= 20) {
+        return {
+          valid: true,
+        }; // not expiring soon
+      } else {
+        return {
+          valid: false,
+        }; // expiring soon
+      }
+    } else {
+      return {
+        valid: false, // pending payment does not exist
+      };
+    }
+  },
+};
